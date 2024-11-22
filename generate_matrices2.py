cfb7bd5bfb5331313cb8fc5e4274d53007ed8d7a
Angel CIMAT <angel.mendez@cimat.mx>
2024-11-21 18:14:05 -0600

    Primer intento con caminatas aleatorias

---
+++ generate_matrices2.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os
+
+def leer_matriz(nombre_archivo):
+    matriz = []
+    with open(nombre_archivo, 'r') as archivo:
+        archivo.readline()
+        archivo.readline()
+        for linea in archivo:
+            fila = [float(valor) for valor in linea.strip().split()]
+            matriz.append(fila)
+    return matriz
+
+years = [2019, 2020, 2021, 2022, 2023, 2024]
+dir1 = '/home/user/Desktop/Datos/Adj_eco/matrices_'
+dir1_1 = '/home/user/Desktop/Datos/Adj_eco_npy/matrices_'
+
+for year in years:
+    dir_temp = dir1 + str(year) + '/'
+    dir_temp2 = dir1_1 + str(year) + '/'
+    if not os.path.exists(dir_temp):
+        os.makedirs(dir_temp)
+    if not os.path.exists(dir_temp2):
+        os.makedirs(dir_temp2)
+    for file in os.listdir(dir_temp):
+        if file.endswith('.txt'):
+            print(file)
+            matriz = np.array(leer_matriz(dir_temp + file))
+            np.save(dir_temp2 + file[:-4], matriz)
