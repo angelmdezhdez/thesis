54ea17a8b7e415e75d5dfb53a6472c7aa803781a
Angel CIMAT <angel.mendez@cimat.mx>
2024-11-22 18:07:02 -0600

    Correcciones

--- gk_a2.py
+++ gk_a2.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from scipy.sparse import csr_matrix, csc_matrix, kron, eye, diags
+import sys
+sys.stdout.flush()
 
 def vector_rf(W, d, f_vec, p_h, node, random_walks=100, h=100):
     '''
@@ -83,6 +85,30 @@ def compute_f_vector(f_alpha, n):
 
     return f
 
+def alpha_laplace(s, n, d = 1):
+    '''
+    This function computes the alpha function for a Laplacian kernel
+    Args:
+        s: Laplacian kernel parameter for regularization
+        n: Number of values to compute
+        d: Default value (power of the degree)
+    Returns:
+        alpha: Alpha function of length n
+    '''
+    alpha = np.ones(n)
+    aux1 = 0
+    aux2 = 1
+    # Recurrent formula
+    q = 1 / (1 + s**(-2))
+    #q = 1
+
+    for i in range(1, n):
+        alpha[i] = ((d + aux1) / aux2) * q * alpha[i-1]
+        aux1 += 1
+        aux2 += 1
+
+    return alpha
+
 def kernel_graph_random_features(Wx, dx, f_vec, Px, Qx, p_h, random_walks=100):
     '''
     Calcula el valor del kernel usando el método de random features
@@ -114,103 +140,6 @@ def count_trips_mibici(data_user, threshold = 5, complement = False):
     viajes_user = viajes_user.sort_values(by = 'prob', ascending = False).reset_index(drop=True)
     return viajes_user
 
-def leer_matriz(nombre_archivo):
-    matriz = []
-    with open(nombre_archivo, 'r') as archivo:
-        archivo.readline()
-        archivo.readline()
-        for linea in archivo:
-            fila = [float(valor) for valor in linea.strip().split()]
-            matriz.append(fila)
-    return matriz
-
-def encontrar_estacion(est, matriz):
-    for i in range(len(matriz)):
-        if matriz[i][0] == est:
-            return matriz[i][1], matriz[i][2]
-    return None, None
-
-def plot_counter(counter_user, est, title, save = False, dir = None):
-    vertex = list(set(counter_user['Est_A'].unique().tolist() + counter_user['Est_B'].unique().tolist()))
-    opacity = np.linspace(0.1, 0.5, len(counter_user))
-    #print(vertex)
-    plt.figure(figsize=(10, 6))
-    for i in vertex:
-        esta = encontrar_estacion(i, est)
-        #print(esta)
-        plt.scatter(esta[1], esta[0], color='blue')
-        plt.text(esta[1] + 0.00001, esta[0] + 0.00001, str(i), fontsize=7, ha='left', va='bottom')
-    for i in range(len(counter_user)):
-        current_trip = counter_user.iloc[i]
-        prob = current_trip["prob"]
-        estA = current_trip["Est_A"]
-        estB = current_trip["Est_B"]
-        if estA == estB:
-            plt.scatter(encontrar_estacion(estA, est)[1], encontrar_estacion(estA, est)[0], color='red', marker='*', s=100)
-        else:
-            aux = np.array([encontrar_estacion(estA, est), encontrar_estacion(estB, est)])
-            plt.plot(aux[:,1], aux[:,0], color='black', alpha=opacity[i])
-    plt.grid()
-    plt.title(f'{title}')def count_trips_mibici(data_user, threshold = 5, complement = False):
-    viajes_user = data_user.groupby([data_user[['Origen_Id', 'Destino_Id']].min(axis=1), data_user[['Origen_Id', 'Destino_Id']].max(axis=1)]).size().reset_index(name='counts')
-    viajes_user.columns = ['Est_A', 'Est_B', 'counts']
-    if not complement:
-        viajes_user = viajes_user[viajes_user['counts'] >= threshold]
-    else:
-        viajes_user = viajes_user[viajes_user['counts'] < threshold]
-    if viajes_user.empty:
-        return None
-    total = viajes_user['counts'].sum()
-    viajes_user['prob'] = viajes_user['counts']/total
-    viajes_user = viajes_user.sort_values(by = 'prob', ascending = False).reset_index(drop=True)
-    return viajes_user
-
-def leer_matriz(nombre_archivo):
-    matriz = []
-    with open(nombre_archivo, 'r') as archivo:
-        archivo.readline()
-        archivo.readline()
-        for linea in archivo:
-            fila = [float(valor) for valor in linea.strip().split()]
-            matriz.append(fila)
-    return matriz
-
-def encontrar_estacion(est, matriz):
-    for i in range(len(matriz)):
-        if matriz[i][0] == est:
-            return matriz[i][1], matriz[i][2]
-    return None, None
-
-def plot_counter(counter_user, est, title, save = False, dir = None):
-    vertex = list(set(counter_user['Est_A'].unique().tolist() + counter_user['Est_B'].unique().tolist()))
-    opacity = np.linspace(0.1, 0.5, len(counter_user))
-    #print(vertex)
-    plt.figure(figsize=(10, 6))
-    for i in vertex:
-        esta = encontrar_estacion(i, est)
-        #print(esta)
-        plt.scatter(esta[1], esta[0], color='blue')
-        plt.text(esta[1] + 0.00001, esta[0] + 0.00001, str(i), fontsize=7, ha='left', va='bottom')
-    for i in range(len(counter_user)):
-        current_trip = counter_user.iloc[i]
-        prob = current_trip["prob"]
-        estA = current_trip["Est_A"]
-        estB = current_trip["Est_B"]
-        if estA == estB:
-            plt.scatter(encontrar_estacion(estA, est)[1], encontrar_estacion(estA, est)[0], color='red', marker='*', s=100)
-        else:
-            aux = np.array([encontrar_estacion(estA, est), encontrar_estacion(estB, est)])
-            plt.plot(aux[:,1], aux[:,0], color='black', alpha=opacity[i])
-    plt.grid()
-    plt.title(f'{title}')
-    if save:
-        directory = f'{dir}/'
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        plt.savefig(f'{directory}/{title}.png')
-        plt.close()
-    else:
-        plt.show()
 
 def compute_matrix(counter_user, normalized = False, self_loops = False):
     if not self_loops:
@@ -231,40 +160,12 @@ def compute_matrix(counter_user, normalized = False, self_loops = False):
         D = np.linalg.inv(np.sqrt(D))
         matrix = np.sqrt(D) @ matrix @ np.sqrt(D)
     return matrix
-    if save:
-        directory = f'{dir}/'
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        plt.savefig(f'{directory}/{title}.png')
-        plt.close()
-    else:
-        plt.show()
 
-def compute_matrix(counter_user, normalized = False, self_loops = False):
-    if not self_loops:
-        counter_user = counter_user[counter_user['Est_A'] != counter_user['Est_B']]
-    vertex = list(set(counter_user['Est_A'].unique().tolist() + counter_user['Est_B'].unique().tolist()))
-    matrix = np.zeros((len(vertex), len(vertex)))
-    for i in range(len(counter_user)):
-        current_trip = counter_user.iloc[i]
-        count = current_trip["counts"]
-        estA = current_trip["Est_A"]
-        estB = current_trip["Est_B"]
-
-        matrix[vertex.index(estA)][vertex.index(estB)] = count
-        matrix[vertex.index(estB)][vertex.index(estA)] = count
-    if normalized:
-        D = np.sum(matrix, axis = 1)
-        D = np.diag(D)
-        D = np.linalg.inv(np.sqrt(D))
-        matrix = np.sqrt(D) @ matrix @ np.sqrt(D)
-    return matrix
 
 data_2019 = pd.read_csv('/home/est_posgrado_angel.mendez/random_walks2/2019.csv')
-estaciones = leer_matriz('/home/est_posgrado_angel.mendez/random_walks2/est_2019.txt')
 
-i1 = np.array([i for i in range(1, 16)])
-i2 = np.array([i for i in range(16, 31)])
+i1 = np.array([i for i in range(1, 3)])
+i2 = np.array([i for i in range(1, 3)])
 
 matrix = np.zeros((len(i1), len(i2)))
 
@@ -272,6 +173,8 @@ for i in range(len(i1)):
     for j in range(len(i2)):
         data1 = data_2019[data_2019['Inicio_del_viaje'].str.startswith(f'2019-01-{i1[i]:02d}')]
         data2 = data_2019[data_2019['Inicio_del_viaje'].str.startswith(f'2019-01-{i2[j]:02d}')]
+        print(f'Calculando para i = {i1[i]}, j = {i2[j]}')
+        sys.stdout.flush()
         counter1 = count_trips_mibici(data1)
         counter2 = count_trips_mibici(data2)
         if counter1 is not None and counter2 is not None:
@@ -282,8 +185,8 @@ for i in range(len(i1)):
             try:
                 mx = kron(m1_s, m2_s, format='csr')
                 dx = np.sum(mx, axis = 1).astype(np.float32)
-                px = (np.ones(len(dx), dtype=np.float32) / len(dx)).astype(np.float32)  
-                qx = (np.ones(len(dx), dtype=np.float32) / len(dx)).astype(np.float32)
+                px = (np.ones(len(dx), dtype=np.float32) / len(dx))  
+                qx = (np.ones(len(dx), dtype=np.float32) / len(dx))
                 f_alpha = lambda n: alpha_laplace(0.18, n, 1).astype(np.float32)
                 f_vec = lambda n: compute_f_vector(f_alpha, n)
                 p_h = np.float32(0.2)
