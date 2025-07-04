b335d73b7dd97023776433dcd2422d072f3ebdf9
Angel <aamdezhdez@gmail.com>
2025-07-04 17:50:30 -0600

    updates

---
+++ ker_aglo_clusters.r
@@ -0,0 +1,94 @@
+##############################################################
+# Libraries
+##############################################################
+
+# Rscript ker_aglo_clusters.r -km /Users/antoniomendez/Desktop/Tesis/thesis/spk_kmeans/test_2/kernel_matrix.npy -odir resultados_pruebas_r -dates /Users/antoniomendez/Desktop/Tesis/thesis/vector_files/2024_fss_jan_apr/dates.csv
+
+options(warn = -1)
+library(argparse)
+library(RcppCNPy)
+library(ggplot2)
+
+##############################################################
+# Argument Parsing
+##############################################################
+
+parser <- ArgumentParser(description = "Clustering aglomerativo con matriz de kernel")
+parser$add_argument('-km',"--kernel_matrix", type = "character", help = "Ruta al archivo .npy de la matriz de kernel", required = TRUE)
+parser$add_argument('-dates', '--dates', type = "character", help = "Fechas a clusterizar", required = TRUE)
+parser$add_argument('-odir', "--outdir", type = "character", default = "resultados", help = "Directorio de salida")
+args <- parser$parse_args()
+
+kernel_path <- args$kernel_matrix   
+if (!file.exists(kernel_path)) {
+  stop("Matrix file does not exist: ", kernel_path)
+}
+
+dates_path <- args$dates
+if (!file.exists(dates_path)) {
+  stop("Dates file does not exist: ", dates)
+}
+
+output_dir <- args$outdir        
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir)
+}
+
+##############################################################
+# Open a file to write results (append mode)
+##############################################################
+
+results_path <- file.path(output_dir, "results.txt")
+results <- file(results_path, open = "w")
+
+cat('Kernel matrix path:', kernel_path, '\n', file = results)
+cat('Output directory:', output_dir, '\n', file = results)
+
+##############################################################
+# Aglomerative Clustering
+##############################################################
+
+start_time <- Sys.time()
+
+K <- npyLoad(kernel_path)
+dates <- read.csv(dates_path, header = TRUE, stringsAsFactors = FALSE)
+rownames(K) <- dates$date
+
+if (nrow(K) != ncol(K)) {
+  stop("Kernel matrix must be square.")
+}
+
+# Computing the distance matrix from the kernel matrix
+D <- sqrt(outer(diag(K), diag(K), "+") - 2 * K)
+
+# dist object
+D_dist <- as.dist(D)
+
+hc <- hclust(D_dist, method = "complete")
+dhc <- as.dendrogram(hc)
+
+end_time <- Sys.time()
+cat('Clustering completed in:', end_time - start_time, 'seconds.\n', file = results)
+
+##########################################################
+# Save clustering results
+##########################################################
+
+n <- nrow(K)
+for (k in 2:(n - 1)) {
+  etiquetas <- cutree(hc, k = k)
+  ruta_etiquetas <- file.path(output_dir, paste0("labels_k", k, ".npy"))
+  npySave(ruta_etiquetas, etiquetas)
+}
+
+pdf(file = file.path(output_dir, "dendrogram.pdf"), width = 18, height = 12)
+
+plot(dhc,
+     main = "Aglomerative clustering dendrogram",
+     xlab = "", sub = "",
+     cex = 0.8)
+
+
+invisible(dev.off())
+
+cat('Finished!\n')
