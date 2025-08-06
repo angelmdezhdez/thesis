720759e0b6536d079a586878d92d7d6613a6e49c
Angel <aamdezhdez@gmail.com>
2025-08-06 17:26:05 -0600

    updates

--- run_many_times.py
+++ run_many_times.py
@@ -6,29 +6,29 @@ import time
 start = time.time()
 
 # Parámetros a variar
-dim_list = [19, 38, 57, 77]
+dim_list = [22, 45, 67, 90]
 regs_list = ['reg', 'noreg']
-input_list = [f'/Users/antoniomendez/Desktop/Tesis/thesis/results_dictionary_learning_mibici/77n_{i}a_{j}' for i in dim_list for j in regs_list]
+input_list = [f'/Users/antoniomendez/Desktop/Tesis/thesis/results_dictionary_learning_mibici/90n_{i}a_{j}' for i in dim_list for j in regs_list]
 
 
 
 # Parámetros fijos
 base_command = [
     "python3", "kmeans_learned_nodes.py",
-    "-int", "[2,26]",
+    "-int", "[2,60]",
     "-st", "/Users/antoniomendez/Desktop/Tesis/Datos/Adj_mibici/matrices_estaciones/est_2024.npy",
-    "-cell", "/Users/antoniomendez/Desktop/Tesis/thesis/station_cells/station_cells_mibici_2024_12.pkl",
-    "-nodes", "/Users/antoniomendez/Desktop/Tesis/thesis/station_cells/nodes_mibici_12.npy",
-    "-index", "12",
+    "-cell", "/Users/antoniomendez/Desktop/Tesis/thesis/station_cells/station_cells_mibici_2024_16.pkl",
+    "-nodes", "/Users/antoniomendez/Desktop/Tesis/thesis/station_cells/nodes_mibici_16.npy",
+    "-index", "16",
     '-sys', 'mibici',
-    "-part", "12"
+    "-part", "16"
 ]
 
 # Ejecutar combinaciones
 for dim, reg, inp in product(dim_list, regs_list, input_list):
     cmd = base_command + [
         "-input", inp,
-        "-out", f"results_kmeans_nodes/mibici_test_kmeans_77_{dim}_{reg}"
+        "-out", f"results_kmeans_nodes/mibici_test_kmeans_90_{dim}_{reg}"
     ]
     print("Ejecutando:", " ".join(cmd))
     subprocess.run(cmd)
--- sparsity_and_dist.py
+++ sparsity_and_dist.py
@@ -37,21 +37,20 @@ if __name__ == "__main__":
     #print(f'Mean distance: {mean_distance(flows, laplacian):.4f}')
     #print(f'Mean sparsity: {mean_sparsity(flows):.4f}')
 
-    n_nodes = [12, 22, 45]
+    n_nodes = [77, 90]
     n_atoms = [
-        [3, 6, 9, 12],
-        [6, 12, 18, 22],
-        [11, 22, 34, 45]
+        [19, 38, 57, 77],
+        [22, 45, 67, 90]
     ]
 
-    file_metrics = "metrics_dist_mibici.txt"
+    file_metrics = "metrics_mibici2.txt"
     with open(file_metrics, "w") as f:
         for i, n_node in enumerate(n_nodes):
             laplacian = np.load(f"results_dictionary_learning_mibici/laplacian_{n_node}.npy")
             for j in range(4):
                 n_atom = n_atoms[i][j]
-                weights_reg = np.load(f"results_dict_dist_learning_mibici/{n_node}n_{n_atom}a_noreg/weights.npy")
-                weights_noreg = np.load(f"results_dict_dist_learning_mibici/{n_node}n_{n_atom}a_reg/weights.npy")
+                weights_noreg = np.load(f"results_dictionary_learning_mibici/{n_node}n_{n_atom}a_noreg/weights.npy")
+                weights_reg = np.load(f"results_dictionary_learning_mibici/{n_node}n_{n_atom}a_reg/weights.npy")
                 f.write(f"{n_node} nodes, {n_atom} atoms, no reg... values: & {mean_distance(weights_noreg, laplacian):.4f}        & {mean_sparsity(weights_noreg):.4f}           & {mean_distance(weights_reg, laplacian):.4f}        & {mean_sparsity(weights_reg):.4f}\n")
 
     print(f"Metrics written to {file_metrics}")
