b2f1d3cb9a680ca2e02b56d5b83c0c535fadad53
Angel <aamdezhdez@gmail.com>
2025-08-19 15:24:39 -0600

    updates

--- multi_projects.py
+++ multi_projects.py
@@ -1,10 +1,14 @@
 import subprocess
+import os
 
-experiments = [
-    ['python3', 'aglo_learned_nodes.py', '-int', '[2,23]', '-st', '/Users/antoniomendez/Desktop/Tesis/Datos/Adj_eco/matrices_estaciones/est_2024.npy', '-cell', 'station_cells/station_cells_ecobici_2024_6.pkl', '-nodes', 'station_cells/nodes_eco_6.npy', '-index', '84', '-sys', 'ecobici', '-part', '6', '-input', 'results_dict_predict_2024/results_results_noreg', '-out', 'results_sequence_days/day_84_noreg'],
-    ['python3', 'aglo_learned_nodes.py', '-int', '[2,23]', '-st', '/Users/antoniomendez/Desktop/Tesis/Datos/Adj_eco/matrices_estaciones/est_2024.npy', '-cell', 'station_cells/station_cells_ecobici_2024_6.pkl', '-nodes', 'station_cells/nodes_eco_6.npy', '-index', '84', '-sys', 'ecobici', '-part', '6', '-input', 'results_dict_predict_2024/results_results_reg', '-out', 'results_sequence_days/day_84_reg'],
-    ['python3', 'aglo_learned_nodes.py', '-int', '[2,23]', '-st', '/Users/antoniomendez/Desktop/Tesis/Datos/Adj_eco/matrices_estaciones/est_2024.npy', '-cell', 'station_cells/station_cells_ecobici_2024_6.pkl', '-nodes', 'station_cells/nodes_eco_6.npy', '-index', '84', '-sys', 'ecobici', '-part', '6', '-input', 'results_dict_predict_2024/results_results_reg_sparse', '-out', 'results_sequence_days/day_84_reg_sparse']
-]
+for i in range(63, 71):
+    experiments = [
+        ['python3', 'aglo_learned_nodes.py', '-int', '[2,23]', '-st', '/Users/antoniomendez/Desktop/Tesis/Datos/Adj_eco/matrices_estaciones/est_2024.npy', '-cell', 'station_cells/station_cells_ecobici_2024_6.pkl', '-nodes', 'station_cells/nodes_eco_6.npy', '-index', str(i), '-sys', 'ecobici', '-part', '6', '-input', 'results_dict_dist_predict_2024/results_noreg', '-out', f'results_dist_sequence_days/day_{i}_noreg'],
+        ['python3', 'aglo_learned_nodes.py', '-int', '[2,23]', '-st', '/Users/antoniomendez/Desktop/Tesis/Datos/Adj_eco/matrices_estaciones/est_2024.npy', '-cell', 'station_cells/station_cells_ecobici_2024_6.pkl', '-nodes', 'station_cells/nodes_eco_6.npy', '-index', str(i), '-sys', 'ecobici', '-part', '6', '-input', 'results_dict_dist_predict_2024/results_reg', '-out', f'results_dist_sequence_days/day_{i}_reg']
+    ]
 
-for cmd in experiments:
-    subprocess.run(cmd)
+    for cmd in experiments:
+        print('Running command:', ' '.join(cmd))
+        subprocess.run(cmd)
+
+os.system(f'curl -d "Projects Finished!!!" ntfy.sh/aamh_091099_ntfy')
